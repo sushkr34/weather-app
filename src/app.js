723a13d6d63f1111cb479cac9cd165ecf8c1ccf9
Sushant Kumar <36587557+sushant-79@users.noreply.github.com>
2020-02-12 13:54:25 +0530

    Updated Query String with Location to fetch weather details

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode =require('./utils/geocode')
+const forecast = require ('./utils/forecast')
 
 const app = express()
 
@@ -40,9 +42,37 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error:'You must provide address'
+        })
+    }
+    geocode (req.query.address,(error,{latitude,longitude,location})=>{
+        if (error){
+            return res.send ({ error})
+        }
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if(error){
+                return res.send({error})
+            }
+            res.send({ 
+                forecast : forecastData,
+                location,
+                address:req.query.address
+            })
+
+        })
+    })    
+})
+
+app.get ('/products',(req,res)=>{
+   if (!req.query.search){
+     return  res.send({
+           error:'You must provide a search '
+       })
+   } 
     res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
+        products :[]
     })
 })
 
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/7d735164bc044f183b5f1c7498354bd5/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
