dfcbc20a9fc0ef036096f1762f8b18ba5e09c792
Sushant Kumar <36587557+sushant-79@users.noreply.github.com>
2020-02-12 16:17:04 +0530

    updated weather app for feeding location value in the ui part instead of query string

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
 console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    fetch('http://localhost:8082/weather?address=' + location).then((response) => {
+        console.log(response)
+    response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+})
